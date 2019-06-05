c5ce5854c7c119fc4e8b03c9951dc9a886dab1d2
Raphael Berly <raphael.berly@blablacar.com>
2019-06-05 22:46:42 +0200

    Add postgres DB support with User model, and login page
    Also add a placeholder for the groups page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
+from conf.app import Config
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+login = LoginManager(app)
 
 from app import routes
