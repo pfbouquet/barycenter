23c866213bf4d6f5d4604551cc35121749ddb699
Hugo Palmer <hugo.palmer@blablacar.com>
2019-06-06 16:53:34 +0200

    get isochrones with launcher example at root

--- launcher_get_isochrones.py
+++ launcher_get_isochrones.py
@@ -1,5 +1,16 @@
+from lib.isochrones import GroupIsochrones
+
+
 def main():
-    print(1)
+    several_isochrones = GroupIsochrones(
+        array_points_lon_lat=[
+            (2.337960+i*0.02, 48.869719) for i in range(2)
+        ],
+        array_transport_mode=['cycling' for i in range(2)]
+    )
+    several_isochrones.compute_isochrones()
+    print(several_isochrones.poi_isochrone_builder)
+
 
 if __name__=='__main__':
     main()
