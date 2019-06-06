64219c75ae42da1429403c214f03c78a0b0c3ddc
Raphael Berly <raphael.berly@blablacar.com>
2019-06-06 12:23:07 +0200

    Change logic of tables (update DDL)

--- ddl/member.sql
+++ ddl/member.sql
@@ -2,9 +2,7 @@ CREATE TABLE coding_night.member (
 
   insert_datetime TIMESTAMP DEFAULT now(),
   id SERIAL NOT NULL CONSTRAINT member_pkey PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  group_id VARCHAR(50) NOT NULL,
-  address_1 VARCHAR(100) NOT NULL,
-  address_2 VARCHAR(100) NOT NULL
+  user_id INTEGER NOT NULL,
+  group_id VARCHAR(50) NOT NULL
 
 );
--- ddl/user.sql
+++ ddl/user.sql
@@ -4,6 +4,8 @@ CREATE TABLE coding_night.user (
   id SERIAL NOT NULL CONSTRAINT users_pkey PRIMARY KEY,
   username VARCHAR(20) NOT NULL,
   password_hash VARCHAR(100) NOT NULL,
-  email TEXT NOT NULL
+  email TEXT NOT NULL,
+  address_1 VARCHAR(100) NOT NULL,
+  address_2 VARCHAR(100) NOT NULL
 
 );
