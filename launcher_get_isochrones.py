e8987ecd6f1c033c5fd0717775b79114ae5812b4
Hugo Palmer <hugo.palmer@blablacar.com>
2019-06-07 14:45:51 +0200

    generates HTML map

--- launcher_get_isochrones.py
+++ launcher_get_isochrones.py
@@ -1,12 +1,13 @@
+from lib.generate_dynamic_map import generate_html_map
 from lib.isochrones import GroupIsochrones
 from dslib.utils.logging import timeit
 import numpy as np
 
 
 def main():
-    size = 10
-    array_transport_mode = ['walking', 'cycling', 'transit', 'transit', 'transit',
-                            'transit', 'walking', 'transit', 'cycling', 'cycling']
+    size = 2
+    array_transport_mode = ['walking', 'cycling']#, 'transit', 'transit', 'transit',
+                            #'transit', 'walking', 'transit', 'cycling', 'cycling']
     with timeit():
         several_isochrones = GroupIsochrones(
             array_points_lon_lat=[
@@ -20,6 +21,14 @@ def main():
     with timeit():
         several_isochrones.compute_isochrones()
     print(several_isochrones.poi_isochrone_builder)
+
+    generate_html_map(
+        dict_isochrones = several_isochrones.poi_isochrone_builder,
+        array_lon_lat_users=[(2.2, 48.8), (2.3, 48.81)],
+        array_popup_users=['Mike house', 'Mike work'],
+        array_lon_lat_bars=[(2.21, 48.81), (2.31, 48.811)],
+        array_popup_bars=['Chez Jacquie', 'Le Favart']
+    )
 
 
 if __name__=='__main__':
