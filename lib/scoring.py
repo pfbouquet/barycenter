b9f3eb3670260cd3cc995bed1bed77a65a3b13cc
Clement Gain <clement.gain@blablacar.com>
2019-06-07 10:28:24 +0200

    global function for scoring places

--- lib/scoring.py
+++ lib/scoring.py
@@ -1,14 +1,15 @@
 from math import log
+import pandas as pd
 
 
-def get_best_time(bar_list):
+def get_best_time(time_list):
 
-    return min([bar_list[i]['time'] for i in range(len(bar_list))])
+    return min(time_list)
 
 
-def get_best_rate(bar_list):
+def get_best_rate(rate_list, number_rate_list):
 
-    return max([bar_list[i]['rate'] * min(log(bar_list[i]['number_of_ratings']), log(200)) for i in range(len(bar_list))])
+    return max([rate_list[i] * min(log(number_rate_list[i]), log(200)) for i in range(len(rate_list))])
 
 
 def get_score(best_rate, min_time, rate, time, price):
@@ -30,6 +31,23 @@ def get_price_score(price):
         return 0.5
     else:
         return 0.25
+
+
+def score_places(place_df):
+
+    best_time = get_best_time(place_df.time)
+    best_rate = get_best_rate(place_df.rating, place_df.review_count)
+
+    list_score = []
+
+    for index, row in place_df.iterrows():
+        current_rate = row['rating'] * min(log(row['review_count']), log(200))
+        current_score = get_score(best_rate, best_time, current_rate, row['time'], row['price'])
+        list_score.append(current_score)
+
+    place_df['score'] = list_score
+
+    return place_df
 
 
 
