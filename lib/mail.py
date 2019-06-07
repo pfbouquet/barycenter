3d070f27aae869536678ae23ca07017adb04d253
pf.bouquet <pierre-francois.bouquet@blablacar.com>
2019-06-07 14:22:57 +0200

    add place info in email preparation

--- lib/mail.py
+++ lib/mail.py
@@ -43,10 +43,11 @@ class MailSender:
     def generate_text(template, **params):
         """
         generate text of the email
-        :param groupname: name of the group
-        :param username: name of the user
+        :param user_name: name of the user
+        :param group_name: name of the group
         :param place_name: name of the place to meet
-        :param place_url: link tio itenary
+        :param place_yelp_url: url to get more details on the bar
+        :param place_direction_url: link to gmaps for direction
         :return: html text to send
         """
         text = render_string(template, params)
@@ -58,8 +59,14 @@ def send_to_group(sender, template, group, place):
     group_name = group['group_name']
     user_list = group['users']
     place_name = place['name']
-    place_url = 'https: // www.google.com / maps / dir /?api = 1 & destination = {latitude}, {longitude} & travelmode = bicycling'.format(**place)
+    place_yelp_url = place['url']
+    place_direction_url = 'https: // www.google.com / maps / dir /?api = 1 & destination = {latitude}, {longitude} & travelmode = bicycling'.format(**place)
 
     for user in user_list:
-        text = sender.generate_text(template, username=user['username'], group_name=group_name, place_name=place_name, place_url=place_url)
+        text = sender.generate_text(template,
+                                    user_name=user['username'],
+                                    group_name=group_name,
+                                    place_name=place_name,
+                                    place_yelp_url=place_yelp_url,
+                                    place_direction_url=place_direction_url)
         sender.send(text, user['email'])
